03c64abfd23f5a739a61a391eca1f81051884d46
akirsteina <akirsteina@users.noreply.github.com>
2021-05-21 09:40:52 +0300

    add nested questions / promises

--- nestedQuestions.js
+++ nestedQuestions.js
@@ -11,11 +11,71 @@ const rl = readline.createInterface({
 // Your data is: ${name} ${lastName} ${email}
 
 // nested questions = callback hell
-rl.question('Enter your first name: ', (firstName) => {
-    rl.question('Enter your last name: ', (lastName) => {
-        rl.question('Enter your email: ', (email) => {
-            console.log(`Your data is: ${firstName} ${lastName} ${email}`);
-            rl.close();
+// rl.question('Enter your first name: ', (firstName) => {
+//     rl.question('Enter your last name: ', (lastName) => {
+//         rl.question('Enter your email: ', (email) => {
+//             console.log(`Your data is: ${firstName} ${lastName} ${email}`);
+//             rl.close();
+//         });
+//     });
+// });
+
+// promises
+new Promise((fulfill, reject) => {
+    rl.question('Enter your first name: ', (firstName) => {
+        if (firstName === '') {
+            reject('First name was empty');
+            return;
+        }
+        fulfill(firstName);
+    });
+}).then((output) => {
+    return new Promise((fulfill, reject) => {
+        rl.question('Enter your last name: ', (lastName) => {
+            if (lastName === '') {
+                reject('Last name was empty');
+                return;
+            }
+            fulfill(`${output} ${lastName}`);
         });
     });
-});
+}).then((output) => {
+    return new Promise((fulfill, reject) => {
+        rl.question('Enter your email: ', (email) => {
+            if (email === '') {
+                reject('Email was empty');
+                return;
+            }
+            fulfill(`${output} ${email}`);
+        });
+    });
+}).then((output) => {
+    console.log(`Your data is: ${output}`);
+}).catch((errorMessage) => {
+    console.log(errorMessage);
+}).finally(() => {
+    rl.close();
+})
+
+// advanced - with objects
+// new Promise((fulfill) => {
+//     rl.question('Enter your first name: ', (firstName) => {
+//         fulfill({ firstName });
+//         // {firstName: firstName}
+//     });
+// }).then((output) => {
+//     return new Promise((fulfill) => {
+//         rl.question('Enter your last name: ', (lastName) => {
+//             fulfill({...output, lastName });
+//         });
+//     });
+// }).then((output) => {
+//     return new Promise((fulfill) => {
+//         rl.question('Enter your email: ', (email) => {
+//             fulfill({...output, email });
+//         });
+//     });
+// }).then(({ firstName, lastName, email }) => {
+//     console.log(`Your data is: ${firstName}, ${lastName}, ${email}`);
+//     rl.close();
+// });
